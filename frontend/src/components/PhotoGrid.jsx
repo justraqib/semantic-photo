f5624793748ce60abce25f997baa1b1d2772a4ee
justraqib <justraqib@gmail.com>
2026-02-24 01:50:22 +0530

    Add PhotoGrid component using responsive Tailwind columns
    - render photos via PhotoCard
    - use grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5
    - pass onPhotoClick down as onOpen callback

---
+++ frontend/src/components/PhotoGrid.jsx
@@ -0,0 +1,15 @@
+import PhotoCard from './PhotoCard';
+
+export default function PhotoGrid({ photos = [], onPhotoClick }) {
+  return (
+    <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      {photos.map((photo) => (
+        <PhotoCard
+          key={photo.id}
+          photo={photo}
+          onOpen={onPhotoClick}
+        />
+      ))}
+    </div>
+  );
+}
